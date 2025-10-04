1ab5fb24bb39a6217088b4f46bdc9ab6a28fb047
Michal Daniel <michaldaniel73@gmail.com>
2025-10-04 20:43:31 +0200

    fix: update storageBucket to correct Firebase URL
    Changed from central-asset-storage.appspot.com to central-asset-storage.firebasestorage.app to match current Firebase Console config.
    
    This fixes Firebase Storage access for all modules including Dashboard.

--- shared/firebase.js
+++ shared/firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 	apiKey: 'AIzaSyDdKzUd-QVHEdHMGl3kbuAKk4p6CjgkgzQ',
 	authDomain: 'central-asset-storage.firebaseapp.com',
 	projectId: 'central-asset-storage',
-	storageBucket: 'central-asset-storage.appspot.com',
+	storageBucket: 'central-asset-storage.firebasestorage.app',
 	messagingSenderId: '907874309868',
 	appId: '1:907874309868:web:5354ee69d6212f3d9937c9'
 };
